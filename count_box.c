d4686598cc3a84d1431375ed791ac863847d3e3d
Luca Ferro <lferro@c2r8s2.42lausanne.ch>
2023-06-17 19:03:55 +0200

    (count_box.c) test print table

--- count_box.c
+++ count_box.c
@@ -1,45 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int count_vue(int *ch)
+
+// calculates visible boxes for view
+int count_view(int *ch)
 {
     int i = 0;
     int count = 0;
 
+    if (ch[i] + ch[i + 1] + ch[i + 2] + ch[i + 3] != 10)
+        return -1;
+
     if (ch[i] == 4)
-        count = 1;
+        return 1;
 
-    if (((ch[i] > ch[i + 1])    && (ch[i] < ch[i + 2])      &&  (ch[i + 2] > ch[i + 3])) 
-    || ((ch[i] > ch[i + 1])     &&  (ch[i] > ch[i + 2])     &&  (ch[i + 1] < ch[i + 3])) 
-    || ((ch[i] < ch[i + 1])     &&  (ch[i + 1] > ch[i + 2]) &&  (ch[i + 2] > ch[i + 3])))
-        count = 2;
+    if (((ch[i] > ch[i + 1]) && (ch[i] < ch[i + 2]) && (ch[i + 2] > ch[i + 3])) 
+    || ((ch[i] > ch[i + 1]) && (ch[i] > ch[i + 2]) && (ch[i] < ch[i + 3])) 
+    || ((ch[i] < ch[i + 1]) && (ch[i + 1] > ch[i + 2]) && (ch[i + 1] > ch[i + 3])))
+        return 2;
 
-    if ((ch[i] > ch[i + 1]      &&  ch[i + 1] > ch[i + 2]   &&  ch[i + 1] < ch[i + 3]) 
-    || (ch[i] < ch[i + 1]       &&  ch[i + 1] > ch[i + 2]   &&  ch[i] < ch[i + 3]) 
-    || (ch[i] < ch[i + 1]       &&  ch[i + 1] < ch[i + 2]       &&  ch[i + 1] > ch[i + 3]))
-        count = 3;
+    if ((ch[i] > ch[i + 1] && ch[i] > ch[i + 2] && ch[i + 2] < ch[i + 3]) 
+    || (ch[i] < ch[i + 1] && ch[i + 1] > ch[i + 2] &&  ch[i + 1] < ch[i + 3]) 
+    || (ch[i] < ch[i + 1] && ch[i + 1] < ch[i + 2] &&  ch[i + 2] > ch[i + 3]))
+        return 3;
 
     if (ch[i] < ch[i + 1] && ch[i + 1] < ch[i + 2] && ch[i + 2] < ch[i + 3])
-        count = 4;
-
-    return count;
+        return 4;
+    return -1;
 }
 
-int main(int argc, char **argv)
+void print_tab(int **arr)
+{
+    int i = 0;
+    while (i < 4)
+    {
+        int j = 0;
+        while (j < 4)
+        {
+            putchar(arr[i][j]);
+            j++;
+        }
+        i++;
+    }
+}
+
+    
+
+// params in main
+int main(void)
+{
+    int r1[4] = {1, 2, 3, 4};
+    int r2[4] = {2, 1, 4, 3};
+    int r3[4] = {3, 4, 1, 2};
+    int r4[4] = {4, 3, 2, 1};
+
+    int *ar[4] = {r1, r2, r3, r4};
+
+    int arr[4][4] = { {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4} };
+
+//    printf("%d\n\n", arr[3][3]);
+        
+//    printf("%d\n", count_view(*ar));
+
+    print_tab(arr);
+
+    return (0);
+}
+
+
+
+
+
+// params as args
+/* int main(int argc, char **argv)
 {
     int i = 0;
     int chain[4];
 
-    while (i < 4)\
+    while (i < 4)
     {
         chain[i] = atoi(argv[i + 1]);
         i++;
     }
-    
-
-//  int chain[4] = {4, 3, 2, 1};
-    
-    printf("%d\n", count_vue(chain));
+        
+    printf("%d\n", count_view(chain));
 
     return (0);
-}
+} */

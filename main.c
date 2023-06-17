be1a45157859926122342d921d7723ce9569f099
Artur Gabasov <agabasov@c2r7s3.42lausanne.ch>
2023-06-17 22:19:10 +0200

    temp

--- main.c
+++ main.c
@@ -1,4 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agabasov <agabasov@student.42lausanne      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/17 22:15:33 by agabasov          #+#    #+#             */
+/*   Updated: 2023/06/17 22:18:36 by agabasov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "lib.h"
+
+void ft_putchar(char c)
+{
+	c = c + 48;
+	write(1, &c, 1);
+}
+
+void print_matrix(int **arr, size_t rows, size_t cols)
+{
+	size_t i = 0;
+	size_t j = 0;
+
+	while (j < rows)
+	{
+		i = 0;
+		while (i < cols)
+		{
+			ft_putchar(arr[j][i]);
+			if (i < cols - 1)
+				write(1, " ", 1);
+			i++;
+		}
+		write(1, "\n", 1);
+		j++;
+	}
+}
 
 int main(void)
 {
@@ -13,11 +51,13 @@ int main(void)
 		matrix[i] = (int *)malloc(cols * sizeof(int));
 		if (matrix[i] == NULL)
 			return 1;
-		if (i == 0) for(size_t j = 0; j<cols; j++)
-			matrix[i][j] = j+1;
+		if (i == 0) 
+			for(size_t j = 0; j<cols; j++)
+				matrix[i][j] = j+1;
 		else
 			permute(matrix[i-1], matrix[i], cols);
 	}
+	print_matrix(matrix, rows, cols);
 
 	//int*	perm_1 = malloc(size * sizeof(int));
 	//int	output[] = permute(perm_0, perm_1, size);

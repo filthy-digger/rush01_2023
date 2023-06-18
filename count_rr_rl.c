dcb0ed5f2d6b2b61b2d3f3b8c1239a015c97fd13
Luca Ferro <lferro@c2r7s2.42lausanne.ch>
2023-06-18 17:25:37 +0200

    cleaned count_rr_rl.c

--- count_rr_rl.c
+++ count_rr_rl.c
@@ -10,7 +10,7 @@ void ft_putchar(char c);
 int count_view(int *ch);
 void print_tab(int arr[16]);
 void print_matrix(int arr[4][4]);
-int transp_arr(int**arr);
+int transpose_arr(int arr[4][4], int transp_arr[4][4]);
 
 
 // count nbr of boxes seen from all POV (WIP for CU and CD)
@@ -19,95 +19,42 @@ void count_rows(int arr[4][4])
     int i = 0;
     int j = 0;
 
-    // create array to store results
     size_t size = 4;
     int *result_counter = malloc(size * sizeof(int));
 
-    // FUNCTION
     // create tranposed array with column values to check
-    
-
-    int r_arr[4][4] = transp_arr(arr);
-    /* int transp_arr[4][4];
-    j = 0;
-    while (j < 4)
-    {
-        transp_arr[0][j] = arr[j][0];
-        transp_arr[1][j] = arr[j][1];
-        transp_arr[2][j] = arr[j][2];
-        transp_arr[3][j] = arr[j][3];
-        j++;
-    } */
-
-
+    int transp_arr[4][4];
+    transpose_arr(arr, transp_arr);
 
     // count C-U
     j = 0;
+    int k = 0;
+    while (j < 4)
+        result_counter[k++] = count_view(transp_arr[j++]);
+
+    // count C-D (reverse tab)
+    j = 0;
     while (j < 4)
     {
-        result_counter[j] = count_view(transp_arr[j]);
-        j++;
+        rev_tab(transp_arr[j], 4);
+        result_counter[k++] = count_view(transp_arr[j++]);
     }
-    
-
-    printf("transp arr : \n");
-    print_matrix(transp_arr);
-
-    // count C-D
-    // reverse tab
-    while (i < 4)
-        rev_tab(transp_arr[i++], 4);
-
-    printf("transp rev arr : \n");
-    print_matrix(transp_arr);
-
-    printf("arr : \n");
-    print_matrix(arr);
-
-    j = 0;
-    while (j < 8)
-    {
-        result_counter[j + 4] = count_view(transp_arr[j]);
-        j++;
-    }
-    
 
     // count R-L
     j = 0;
-    while (j < 12)
-    {
-        result_counter[j + 8] = count_view(arr[j]);
-        j++;
-    }
+    while (j < 4)
+        result_counter[k++] = count_view(arr[j++]);
 
-    // count R-R
-    // reverse tab
-    i = 0;
-    while (i < 4)
-        rev_tab(arr[i++], 4);
-
-
-     printf("rev arr : \n");
-    print_matrix(arr);
-
-
+    // count R-R (reverse tab)
     j = 0;
-    while (j < 16)
+    while (j < 4)
     {
-        result_counter[j + 12] = count_view(arr[j]);
-        j++;
+        rev_tab(arr[j], 4);
+        result_counter[k++] = count_view(arr[j++]);
     }
 
-    
-
-     //       counts_cu[j] = count_view(&arr[i][j]);
-
-    // PRINTING FOR TESTING 
    print_tab(result_counter);
    write(1, "\n", 1);
- //  print_tab(counts_cd);
-
-
 }
 
 

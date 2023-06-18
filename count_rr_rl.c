172010a71aa72a5cdc00053894182c56f32d7a9a
Luca Ferro <lferro@c2r7s2.42lausanne.ch>
2023-06-18 15:20:34 +0200

    working checker, but garbage synthax

--- count_rr_rl.c
+++ count_rr_rl.c
@@ -3,55 +3,14 @@
 #include <unistd.h>
 
 
-// import
+// imports
 void rev_tab(int *tab, int size);
 void count_row(int arr[4][4]);
+void ft_putchar(char c);
+int count_view(int *ch);
+void print_tab(int arr[16]);
+void print_matrix(int arr[4][4]);
 
-
-// print numbers (char) from int
-void ft_putchar(char c)
-{
-    c = c + 48;
-    write(1, &c, 1);
-}
-
-// count visible boxes logic for a row
-int count_view(int *ch)
-{
-    int i = 0;
-    int count = 0;
-
-    if (ch[i] + ch[i + 1] + ch[i + 2] + ch[i + 3] != 10)
-        return -1;
-
-    if (ch[i] == 4)
-        return 1;
-
-    if (((ch[i] > ch[i + 1]) && (ch[i] < ch[i + 2]) && (ch[i + 2] > ch[i + 3])) 
-    || ((ch[i] > ch[i + 1]) && (ch[i] > ch[i + 2]) && (ch[i] < ch[i + 3])) 
-    || ((ch[i] < ch[i + 1]) && (ch[i + 1] > ch[i + 2]) && (ch[i + 1] > ch[i + 3])))
-        return 2;
-
-    if ((ch[i] > ch[i + 1] && ch[i] > ch[i + 2] && ch[i + 2] < ch[i + 3]) 
-    || (ch[i] < ch[i + 1] && ch[i + 1] > ch[i + 2] &&  ch[i + 1] < ch[i + 3]) 
-    || (ch[i] < ch[i + 1] && ch[i + 1] < ch[i + 2] &&  ch[i + 2] > ch[i + 3]))
-        return 3;
-
-    if (ch[i] < ch[i + 1] && ch[i + 1] < ch[i + 2] && ch[i + 2] < ch[i + 3])
-        return 4;
-    return -1;
-}
-
-// print 4x4 matrix
-
-
-// print 1d tab for testing purpose
-void print_tab(int arr[16])
-{
-    int i = 0;
-    while (i < 4)
-            ft_putchar(arr[i++]);
-}
 
 
 // count nbr of boxes seen from all POV (WIP for CU and CD)
@@ -59,12 +18,10 @@ void count_rows(int arr[4][4])
 {
     int i = 0;
     int j = 0;
-    // create array to store results
-    int counts_rl[4] = {}; 
-    int counts_rr[4] = {}; 
-    int counts_cu[4] = {}; 
-    int counts_cd[4] = {}; 
 
+    // create array to store results
+    size_t size = 4;
+    int *result_counter = malloc(size * sizeof(int));
 
     // FUNCTION
     // create tranposed array with column values to check
@@ -80,39 +37,69 @@ void count_rows(int arr[4][4])
     }
 
     // count C-U
-    while (j < 4)
-    {
-        counts_cu[j] = count_view(transp_arr[j]);
-        j++;
-    }
-    // count C-D
-    // reverse tab
-    i = 0;
-    while (i < 4)
-        rev_tab(transp_arr[i++], 4);
-
-    write(1, "\n", 1);
-
     j = 0;
     while (j < 4)
     {
-        counts_cd[j] = count_view(transp_arr[j]);
+        result_counter[j] = count_view(transp_arr[j]);
         j++;
     }
     
 
-    count_row(arr[4][4]);
+    printf("transp arr : \n");
+    print_matrix(transp_arr);
+
+    // count C-D
+    // reverse tab
+    while (i < 4)
+        rev_tab(transp_arr[i++], 4);
+
+    printf("transp rev arr : \n");
+    print_matrix(transp_arr);
+
+    printf("arr : \n");
+    print_matrix(arr);
+
+    j = 0;
+    while (j < 8)
+    {
+        result_counter[j + 4] = count_view(transp_arr[j]);
+        j++;
+    }
+    
+
+    // count R-L
+    j = 0;
+    while (j < 12)
+    {
+        result_counter[j + 8] = count_view(arr[j]);
+        j++;
+    }
+
+    // count R-R
+    // reverse tab
+    i = 0;
+    while (i < 4)
+        rev_tab(arr[i++], 4);
+
+
+     printf("rev arr : \n");
+    print_matrix(arr);
+
+
+    j = 0;
+    while (j < 16)
+    {
+        result_counter[j + 12] = count_view(arr[j]);
+        j++;
+    }
+
     
 
      //       counts_cu[j] = count_view(&arr[i][j]);
 
     // PRINTING FOR TESTING 
-   print_tab(counts_rl);
+   print_tab(result_counter);
    write(1, "\n", 1);
-   print_tab(counts_rr);
-   write(1, "\n", 1);
-   print_tab(counts_cu);
-    write(1, "\n", 1);
  //  print_tab(counts_cd);
 
 
@@ -125,7 +112,7 @@ void count_rows(int arr[4][4])
 // params in main
 int main(void)
 {
-    int arr[4][4] = {{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}};
+    int arr[4][4] = {{1, 2, 3, 4}, {2, 1, 4 ,3}, {4, 3, 1, 2}, {3, 4, 2, 1}};
 
     count_rows(arr);
 
--- lib2.c
+++ lib2.c
@@ -1,3 +1,10 @@
+#include "includes.h"
+
+void ft_putchar(char c)
+{
+	c = c + 48;
+	write(1, &c, 1);
+}
 void	ft_swap(int *a, int*b)
 {
 	int	temp;
@@ -42,7 +49,7 @@ void print_matrix(int arr[4][4])
     }
 }
 
-int    transp_matrix(arr[4][4])
+void    transp_matrix(int arr[4][4])
 {
     int r_arr[4][4];
     int j = 0;
@@ -56,12 +63,41 @@ int    transp_matrix(arr[4][4])
     }
 }
 
+int count_view(int *ch)
+{
+    int i = 0;
+    int count = 0;
+
+    if (ch[i] + ch[i + 1] + ch[i + 2] + ch[i + 3] != 10)
+        return -1;
+
+    if (ch[i] == 4)
+        return 1;
+
+    if (((ch[i] > ch[i + 1]) && (ch[i] < ch[i + 2]) && (ch[i + 2] > ch[i + 3])) 
+    || ((ch[i] > ch[i + 1]) && (ch[i] > ch[i + 2]) && (ch[i] < ch[i + 3])) 
+    || ((ch[i] < ch[i + 1]) && (ch[i + 1] > ch[i + 2]) && (ch[i + 1] > ch[i + 3])))
+        return 2;
+
+    if ((ch[i] > ch[i + 1] && ch[i] > ch[i + 2] && ch[i + 2] < ch[i + 3]) 
+    || (ch[i] < ch[i + 1] && ch[i + 1] > ch[i + 2] &&  ch[i + 1] < ch[i + 3])
+    || (ch[i] > ch[i + 1] && ch[i + 1] < ch[i + 2] &&  ch[i + 1] < ch[i + 3])
+    || (ch[i] < ch[i + 1] && ch[i + 1] < ch[i + 2] &&  ch[i + 2] > ch[i + 3]))
+        return 3;
+
+    if (ch[i] < ch[i + 1] && ch[i + 1] < ch[i + 2] && ch[i + 2] < ch[i + 3])
+        return 4;
+    return -1;
+}
+
+
 
 
 void count_row(int arr[4][4])
 {
-    int counts_rl[4] = {}; 
-    int counts_rr[4] = {}; 
+    size_t size = 4;
+    int *counts_rl = malloc(size * sizeof(int)); 
+    int *counts_rr = malloc(size * sizeof(int)); 
     int j = 0;
 
     // count R-L
@@ -73,7 +109,7 @@ void count_row(int arr[4][4])
 
     // count R-R
     // reverse tab
-    i = 0;
+    int i = 0;
     while (i < 4)
         rev_tab(arr[i++], 4);
 
@@ -85,6 +121,13 @@ void count_row(int arr[4][4])
         counts_rr[j] = count_view(arr[j]);
         j++;
     }
+}
+
+void print_tab(int arr[16])
+{
+    int i = 0;
+    while (i < 16)
+        ft_putchar(arr[i++]);
 }
 
 /* void    count_col(int arr[4][4])

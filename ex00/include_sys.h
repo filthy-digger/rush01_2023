376c4aa018a4a1864fb4461f2e3a4c8b96cb6217
agabasov <agabasov@student.42lausanne.ch>
2023-06-21 11:00:04 +0200

    norminette

---
+++ ex00/include_sys.h
@@ -0,0 +1,13 @@
+//
+// Created by Artur Gabasov on 6/21/23.
+//
+
+#ifndef RUSH01_INCLUDE_SYS_H
+#define RUSH01_INCLUDE_SYS_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#endif //RUSH01_INCLUDE_SYS_H
--- ex00/lib.h
+++ ex00/lib.h
@@ -1,57 +1,66 @@
-#include "includes.h"
 
-void	ft_putnbr(int nb);
+//
+// Created by Artur Gabasov on 6/21/23.
+//
 
-size_t ft_strlen(char *str);
+#ifndef RUSH01_LIB_H
+#define RUSH01_LIB_H
+#include "include_sys.h"
 
-int *permute(int *prev, int *next, size_t size);
+void			ft_putnbr(int nb);
 
-int ft_power(int b, unsigned int e);
+size_t			ft_strlen(char *str);
 
-unsigned int factorial(unsigned int n);
+int				*permute(int *prev, int *next, size_t size);
 
-void swap_int(int *ptr1, int *ptr2);
+int				ft_power(int b, unsigned int e);
 
-int *rev_int_arr(int *arr, size_t size);
+unsigned int	factorial(unsigned int n);
 
-void ft_putchar(char c);
+void			swap_int(int *ptr1, int *ptr2);
 
-int count_view(const int *ch);
+int				*rev_int_arr(int *arr, size_t size);
 
-int **transpose_matrix(int **matrix, int **matrix_transpose, size_t rows,
-					   size_t cols);
+void			ft_putchar(char c);
 
-//col1up col2up col3up col4up col1down col2down col3down col4down row1left
+int				count_view(const int *ch);
+
+int				**transpose_matrix(int **matrix, int **matrix_transpose,
+					size_t rows, size_t cols);
+
+// col1up col2up col3up col4up col1down col2down col3down col4down row1left
 // row2left row3left row4left row1right row2right row3right row4right
 //"4 3 2 1 1 2 2 2 4 3 2 1 1 2 2 1"
 
-//parse_uinput takes:
+// parse_uinput takes:
 //"str" - a pointer to a NUL-terminated string
 //"n" - square side length
 //
-//returns a boolean:
-//true if the string follows the rush01 format
-//false otherwise
+// returns a boolean:
+// true if the string follows the rush01 format
+// false otherwise
 //
-//format is:
+// format is:
 //"space_count" = n^2 - 1
-//length =  n^2  + "space_count"
-int *parse_uinput(char* str, unsigned int n);
+// length =  n^2  + "space_count"
+int				*parse_uinput(char *str, unsigned int n);
 
-void print_matrix(int **arr, size_t rows, size_t cols);
+void			print_matrix(int **arr, size_t rows, size_t cols);
 
-void puterr(char* error_detail);
+void			puterr(char *error_detail);
 
-int checker(const int *input, const int *result, size_t size);
+int				checker(const int *input, const int *result, size_t size);
 
-void count_rows(int **matrix, int *dest_arr, size_t size);
+void			count_rows(int **matrix, int *dest_arr, size_t size);
 
-bool sudoku(int **matrix, size_t n);
+bool			sudoku(int **matrix, size_t n);
 
-int **gen_permutations(size_t size);
+int				**gen_permutations(size_t size);
 
-int** malloc_matrix(size_t cols, size_t rows);
+int				**malloc_matrix(size_t cols, size_t rows);
 
-int **malloc_matrix_rows(size_t rows);
+int				**malloc_matrix_rows(size_t rows);
 
-int **gen_solution(size_t size, int* input);
+int				**gen_solution(size_t size, int *input);
+
+#endif //RUSH01_LIB_H
